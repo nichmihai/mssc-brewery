1dd6e61ee5c7b5fb4686540105cd06083f33cb86
nichmihai <nichmihai4@gmail.com>
2024-05-28 22:31:47 -0400

    Adding Cutsomer Dto

---
+++ src/main/java/nich/mihai/msscbrewery/web/model/CustomerDto.java
@@ -0,0 +1,17 @@
+package nich.mihai.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class CustomerDto {
+    private UUID customerId;
+    private String customerName;
+}
