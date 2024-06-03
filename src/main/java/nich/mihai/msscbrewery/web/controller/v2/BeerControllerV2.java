44b4ae8bb4446bc72b5560e6d6b3da0fcf90a5cb
nichm <nichmihai4@gmail.com>
2024-06-02 20:54:47 -0400

    Adding the V2 version of Beer

---
+++ src/main/java/nich/mihai/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -0,0 +1,54 @@
+package nich.mihai.msscbrewery.web.controller.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import nich.mihai.msscbrewery.services.BeerService;
+import nich.mihai.msscbrewery.services.v2.BeerServiceV2;
+import nich.mihai.msscbrewery.web.model.BeerDto;
+import nich.mihai.msscbrewery.web.model.v2.BeerDtoV2;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v2/beer")
+public class BeerControllerV2 {
+    private final BeerServiceV2 beerService;
+
+    public BeerControllerV2(BeerServiceV2 beerService) {
+        this.beerService = beerService;
+    }
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId) {
+        return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody BeerDtoV2 beerDto) {
+        BeerDtoV2 savedDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "http://localhost:8080/api/v2/beer/" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDtoV2 beerDto) {
+
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+        beerService.deleteById(beerId);
+        log.debug("Deleting beer..." + beerId.toString());
+    }
+}
